48bc21a9694d84bd5cf206584c47021bc348b56e
Arsen Temp <arsentemp@Sonas-MacBook-Pro.local>
2020-04-20 23:38:34 +0400

    daily calories dialog redesigned

--- app/src/main/java/com/example/inputofcalories/presentation/regularflow/home/SetDailyCaloriesDialog.kt
+++ app/src/main/java/com/example/inputofcalories/presentation/regularflow/home/SetDailyCaloriesDialog.kt
@@ -2,7 +2,10 @@ package com.example.inputofcalories.presentation.regularflow.home
 
 import android.app.Dialog
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.Window
 import androidx.lifecycle.MutableLiveData
 import com.example.inputofcalories.R
 import kotlinx.android.synthetic.main.dialog_set_daily_calories.*
@@ -12,16 +15,28 @@ class SetDailyCaloriesDialog(context: Context): Dialog(context) {
     val dailyCaloriesLiveData = MutableLiveData<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTransparentBackground()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dialog_set_daily_calories)
 
         setupClickListeners()
     }
 
+    private fun setTransparentBackground() {
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window?.run { setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT)) }
+    }
+
     private fun setupClickListeners() {
         applyButton.setOnClickListener {
-            dailyCaloriesLiveData.value = dailyCaloriesEditText.text.toString()
-            dismiss()
+            val text = dailyCaloriesEditText.text.toString()
+            if(text.isNotBlank()) {
+                dailyCaloriesLiveData.value = text
+                dismiss()
+            } else {
+                dailyCaloriesEditText.error = context.getString(R.string.please_set_daily_calories)
+            }
         }
     }
 }
